16dfb7fe1bc62877778fe78d026447a5cd5827f0
some-evil <96083130+some-evil@users.noreply.github.com>
2021-12-15 22:35:16 +0300

    1.0

---
+++ lab1/lab1/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.encodeBt = new System.Windows.Forms.Button();
+            this.decodeBt = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.encodeTextBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.clearBt = new System.Windows.Forms.Button();
+            this.loadTextBt = new System.Windows.Forms.Button();
+            this.statusLb = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.loadEncodeText = new System.Windows.Forms.Button();
+            this.setKeyBt = new System.Windows.Forms.Button();
+            this.keyBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // encodeBt
+            // 
+            this.encodeBt.Location = new System.Drawing.Point(13, 12);
+            this.encodeBt.Name = "encodeBt";
+            this.encodeBt.Size = new System.Drawing.Size(75, 23);
+            this.encodeBt.TabIndex = 0;
+            this.encodeBt.Text = "Encode";
+            this.encodeBt.UseVisualStyleBackColor = true;
+            this.encodeBt.Click += new System.EventHandler(this.encode_Click);
+            // 
+            // decodeBt
+            // 
+            this.decodeBt.Location = new System.Drawing.Point(175, 12);
+            this.decodeBt.Name = "decodeBt";
+            this.decodeBt.Size = new System.Drawing.Size(75, 23);
+            this.decodeBt.TabIndex = 1;
+            this.decodeBt.Text = "Decode";
+            this.decodeBt.UseVisualStyleBackColor = true;
+            this.decodeBt.Click += new System.EventHandler(this.decode_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 64);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(202, 111);
+            this.textBox1.TabIndex = 2;
+            // 
+            // encodeTextBox2
+            // 
+            this.encodeTextBox2.Location = new System.Drawing.Point(221, 64);
+            this.encodeTextBox2.Multiline = true;
+            this.encodeTextBox2.Name = "encodeTextBox2";
+            this.encodeTextBox2.Size = new System.Drawing.Size(202, 111);
+            this.encodeTextBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(218, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Encode text";
+            // 
+            // clearBt
+            // 
+            this.clearBt.Location = new System.Drawing.Point(94, 12);
+            this.clearBt.Name = "clearBt";
+            this.clearBt.Size = new System.Drawing.Size(75, 23);
+            this.clearBt.TabIndex = 6;
+            this.clearBt.Text = "Clear";
+            this.clearBt.UseVisualStyleBackColor = true;
+            this.clearBt.Click += new System.EventHandler(this.clear_click);
+            // 
+            // loadTextBt
+            // 
+            this.loadTextBt.Location = new System.Drawing.Point(12, 181);
+            this.loadTextBt.Name = "loadTextBt";
+            this.loadTextBt.Size = new System.Drawing.Size(76, 23);
+            this.loadTextBt.TabIndex = 7;
+            this.loadTextBt.Text = "Load text";
+            this.loadTextBt.UseVisualStyleBackColor = true;
+            this.loadTextBt.Click += new System.EventHandler(this.loadFile_Click);
+            // 
+            // statusLb
+            // 
+            this.statusLb.AutoSize = true;
+            this.statusLb.Location = new System.Drawing.Point(347, 226);
+            this.statusLb.Name = "statusLb";
+            this.statusLb.Size = new System.Drawing.Size(16, 13);
+            this.statusLb.TabIndex = 8;
+            this.statusLb.Text = " 1";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 225);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(71, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "ShowKey";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // loadEncodeText
+            // 
+            this.loadEncodeText.Location = new System.Drawing.Point(221, 181);
+            this.loadEncodeText.Name = "loadEncodeText";
+            this.loadEncodeText.Size = new System.Drawing.Size(103, 23);
+            this.loadEncodeText.TabIndex = 10;
+            this.loadEncodeText.Text = "Load encode text";
+            this.loadEncodeText.UseVisualStyleBackColor = true;
+            this.loadEncodeText.Click += new System.EventHandler(this.loadEncodeFile_Click);
+            // 
+            // setKeyBt
+            // 
+            this.setKeyBt.Location = new System.Drawing.Point(293, 221);
+            this.setKeyBt.Name = "setKeyBt";
+            this.setKeyBt.Size = new System.Drawing.Size(48, 23);
+            this.setKeyBt.TabIndex = 12;
+            this.setKeyBt.Text = "Set";
+            this.setKeyBt.UseVisualStyleBackColor = true;
+            this.setKeyBt.Visible = false;
+            this.setKeyBt.Click += new System.EventHandler(this.setKeyBt_Click);
+            // 
+            // keyBox
+            // 
+            this.keyBox.Location = new System.Drawing.Point(84, 223);
+            this.keyBox.Name = "keyBox";
+            this.keyBox.Size = new System.Drawing.Size(202, 20);
+            this.keyBox.TabIndex = 13;
+            this.keyBox.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 260);
+            this.Controls.Add(this.keyBox);
+            this.Controls.Add(this.setKeyBt);
+            this.Controls.Add(this.loadEncodeText);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.statusLb);
+            this.Controls.Add(this.loadTextBt);
+            this.Controls.Add(this.clearBt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.encodeTextBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.decodeBt);
+            this.Controls.Add(this.encodeBt);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Enigma";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button encodeBt;
+        private System.Windows.Forms.Button decodeBt;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox encodeTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button clearBt;
+        private System.Windows.Forms.Button loadTextBt;
+        private System.Windows.Forms.Label statusLb;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button loadEncodeText;
+        private System.Windows.Forms.Button setKeyBt;
+        private System.Windows.Forms.TextBox keyBox;
+    }
+}
+
---
+++ lab1/lab1/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        String alphabet = "abcdefghijklmnopqrstuvwxyz1234567890 !@#$%^&*()_+=-<>.,`~\"\'|\\/?";
+        //String revhabet = "@#$%^&*()_+=-<>.,`~\"\'|\\/?qwertyuiopasdfghjklzxcvbnm 0987654321!";
+        String revhabet;
+        String normallyT;
+        String decodeT;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            revhabet = genKey(alphabet);
+            keyBox.Text = revhabet;
+        }
+
+        public static string getStep(string t)
+        {
+            //
+            return t.Substring(1, t.Length - 1) + t.Substring(0, 1);
+        }
+        
+
+
+        private string enigma(String text, String outside, String inside)
+        {
+            String outText = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < outside.Length; j++)
+                {
+                    Console.WriteLine(text.Length);
+                    if (text[i] == outside[j])
+                    {
+                        outText += inside[j];
+                        //inside = getStep(inside);
+                        break;
+                    }
+                }
+            }
+            return outText;
+        }
+
+        private string genKey(String str)
+        {
+            char[] array = str.ToCharArray();
+            Random rng = new Random();
+            int n = array.Length;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                var value = array[k];
+                array[k] = array[n];
+                array[n] = value;
+            }
+            return new string(array);
+        }
+
+        private void encode_Click(object sender, EventArgs e)
+        {   
+            if (checkBox1.Checked == true)
+            {
+                //text from file
+            }
+            normallyT = textBox1.Text.ToLower();
+            encodeTextBox2.Text = enigma(normallyT, alphabet, revhabet);
+        }
+
+        private void decode_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                //text from file
+            }
+            decodeT = encodeTextBox2.Text;
+            textBox1.Text = enigma(decodeT, revhabet, alphabet);
+        }
+
+        private void clear_click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            encodeTextBox2.Clear();
+            //OpenFileDialog ofd = new OpenFileDialog();
+            //if (ofd.ShowDialog() == DialogResult.Cancel)
+            //    return;
+            //MessageBox.Show(ofd.FileName.Replace(".txt", "Decoded.txt"));
+            //revhabet = getStep(revhabet);
+            //MessageBox.Show(revhabet);
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox checkBox = (CheckBox)sender;
+            if (checkBox.Checked == true)
+            {
+                keyBox.Visible = true;
+                setKeyBt.Visible = true;
+            }
+            else
+            {
+                keyBox.Visible = false;
+                setKeyBt.Visible = false;
+            }
+        }
+
+        private void loadFile_Click(object sender, EventArgs e)
+        {
+            statusLb.Text = "Loading";
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.Cancel)
+                return;
+            textBox1.Text = System.IO.File.ReadAllText(ofd.FileName);
+            decodeT = textBox1.Text.ToLower();
+            normallyT = enigma(decodeT, alphabet, revhabet);
+            encodeTextBox2.Text = normallyT;
+            //ofd.FileName.Replace(".txt", "Decoded.txt");
+            System.IO.File.WriteAllText(ofd.FileName.Replace(".txt", "_ENCODED.txt"), normallyT);
+            statusLb.Text = "Complete";
+        }
+
+        private void loadEncodeFile_Click(object sender, EventArgs e)
+        {
+            statusLb.Text = "Loading";
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.Cancel)
+                return;
+            encodeTextBox2.Text = System.IO.File.ReadAllText(ofd.FileName);
+            normallyT = encodeTextBox2.Text;
+            decodeT = enigma(normallyT, revhabet, alphabet);
+            textBox1.Text = decodeT;
+            System.IO.File.WriteAllText(ofd.FileName.Replace(".txt", "_DECODED.txt"), decodeT);
+            statusLb.Text = "Complete";
+        }
+
+        private void setKeyBt_Click(object sender, EventArgs e)
+        {
+            if (keyBox.Text == "")
+            {
+                keyBox.Text = genKey(alphabet);
+                revhabet = keyBox.Text;
+                statusLb.Text = "Key generated";
+            } else
+            {
+                revhabet = keyBox.Text;
+                statusLb.Text = "Key set";
+            }
+            
+        }
+
+        private void keyBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
